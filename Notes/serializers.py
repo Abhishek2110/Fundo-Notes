8cc2be42d208d2cf44cf68040185a16c0246c095
Abhishek Rajpal <86553778+Abhishek2110@users.noreply.github.com>
2024-02-19 16:21:11 +0530

    [Abhishek] Added . Archive and Trash API

--- Notes/serializers.py
+++ Notes/serializers.py
@@ -4,7 +4,8 @@ from .models import Notes, Label
 class NotesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notes
-        fields = ('id', 'title', 'description', 'color', 'user')
+        fields = ('id', 'title', 'description', 'color', 'is_archive', 'is_trash', 'user')
+        read_only_fields = ['is_archive', 'is_trash']
     
 class LabelSerializer(serializers.ModelSerializer):
     class Meta:
--- Notes/urls.py
+++ Notes/urls.py
@@ -1,15 +1,26 @@
 from django.urls import path
 from . import views
-from .views import LabelAPI
 
-my_viewset = LabelAPI.as_view({
+my_viewset = views.LabelAPI.as_view({
     'get': 'get',
     'post': 'post',
     'put': 'put',
-    'delete': 'delete'
+    'delete': 'delete',
+})
+
+archive = views.ArchiveTrashAPI.as_view({
+    'patch': 'update_archive',
+    'get': 'get_archived_notes'    
+})
+
+trash = views.ArchiveTrashAPI.as_view({
+    'patch': 'update_trash',    
+    'get': 'get_trash_notes'
 })
 
 urlpatterns = [
     path('notes/', views.NotesAPI.as_view(), name='NotesApi'),
     path('label/', my_viewset, name = 'LabelApi'),
+    path('archive/', archive, name=''),
+    path('trash/', trash, name='')
 ]
