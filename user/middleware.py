30d1473aa8525e0e6e56bc473e3a5491e093afaf
Abhishek Rajpal <86553778+Abhishek2110@users.noreply.github.com>
2024-02-28 17:12:08 +0530

    [Abhishek] Added . Middleware

---
+++ user/middleware.py
@@ -0,0 +1,22 @@
+from .models import RequestLog
+
+class RequestLogMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+        log = RequestLog.objects.filter(path=request.path, method=request.method).first()
+        if log:
+            log.count += 1
+            log.save()
+        else:
+            RequestLog.objects.create(path=request.path, method=request.method)
+        response = self.get_response(request)
+        print("middleware after response")
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
--- user/models.py
+++ user/models.py
@@ -4,3 +4,9 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     is_verified = models.BooleanField(default=False)
+    
+    
+class RequestLog(models.Model):
+    method = models.CharField(max_length=10, null=False)
+    path = models.CharField(max_length=255, null=False)
+    count = models.BigIntegerField(default=1)
